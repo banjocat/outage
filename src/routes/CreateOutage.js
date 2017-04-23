ad2734367a2097562556b0068eb7f37ab6cfde65
Banjocat <jackmuratore@gmail.com>
2017-04-22 22:22:28 -0400

    Add select and choice box to template demo

--- src/routes/CreateOutage.js
+++ src/routes/CreateOutage.js
@@ -2,6 +2,7 @@ import React, {Component}  from 'react';
 import Dashboard from './shared/Dashboard';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
+import Checkbox from 'material-ui/Checkbox';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
 import Paper from 'material-ui/Paper';
@@ -28,10 +29,36 @@ class CreateOutage extends Component {
                 },
             ]
             },
+            {id: 2, name: 'Complex Outage', fields: [
+                {id: 1,
+                    type: 'TextField',
+                    name: 'title',
+                    hintText: 'Outage Title',
+                    errorText: 'This field is required'
+                },
+                {id: 2,
+                    type: 'TextField',
+                    name: 'short-descritpion',
+                    multiLine: true,
+                    hintText: 'Outage Short Description',
+                    errorText: 'This field is required'
+                },
+                {id: 3,
+                    type: 'Choices',
+                    name: 'Reporter',
+                    choices: ['Jack', 'Cassie', 'Walter', 'Baby'],
+                },
+                {id: 4,
+                    type: 'Checkbox',
+                    name: 'Check box',
+                    choices: ['Check1', 'Check2', 'Check3']
+                },
+            ]
+            },
         ]
         this.state = {
             templates: templates,
-            pickedTemplate: 0,
+            pickedTemplate: 2,
         }
     }
     componentDidMount() {
@@ -40,14 +67,18 @@ class CreateOutage extends Component {
     componentWillUnmount() {
     }
 
+    changeTemplate = (event, index, value) => {
+        this.setState({'pickedTemplate': value});
+    }
     TemplatePicker = () => {
+        const chosenId = this.state.pickedTemplate;
         const choices = this.state.templates.map( (template) => {
             return (
-                <MenuItem key={template.id} primaryText={template.name} />
+                <MenuItem key={template.id} value={template.id} checked={chosenId === template.id} primaryText={template.name} />
             )
         });
         return (
-            <SelectField>
+            <SelectField floatingLabelText="Template" onChange={this.changeTemplate} value={chosenId} >
                 {choices}
             </SelectField>
         )
@@ -57,34 +88,55 @@ class CreateOutage extends Component {
         if (value.type === 'TextField') {
             return (
                 <div key={value.id}>
-                <TextField
-                    key={value.id}
-                    name={value.name}
-                    hintText={value.hintText}
-                    errorText={value.errorText}
-                    multiLine={value.multiLine}
-                />
-                <br />
-                <br />
-            </div>
+                    <TextField
+                        key={value.id}
+                        name={value.name}
+                        hintText={value.hintText}
+                        errorText={value.errorText}
+                        multiLine={value.multiLine}
+                    />
+                    <br />
+                    <br />
+                </div>
+            );
+        }
+        else if (value.type === 'Choices') {
+            const choices = _.map(value.choices, (choice, key) => {
+                return (
+                <MenuItem key={key} value={choice} primaryText={choice} />
+                )
+            });
+            return (
+                <SelectField key={value.id} floatingLabelText={value.name}  >
+                    {choices}
+                </SelectField>
+            );
+        }
+        else if ( value.type === 'Checkbox' ) {
+            const choices = _.map(value.choices, (choice, key) => {
+                return (
+                <Checkbox key={key} value={choice} label={choice} />
+                )
+            });
+            return (
+                <SelectField key={value.id} floatingLabelText={value.name}  >
+                    {choices}
+                </SelectField>
             );
         }
     }
 
     GetPickedTemplateFields = () => {
         // Default to first one
-        let template = this.state.templates[0];
-        _.each(this.state.templates, (item) => {
-            if  item.id == this.state.pickedTemplate {
-                template = item;
-            }
+        let index = _.findIndex(this.state.templates, (item) => {
+            return (item.id === this.state.pickedTemplate);
         });
-        return template;
+        return this.state.templates[index]['fields'];
     }
 
     Form = () => {
-        const index = this.state.pickedTemplate;
-        const jsx = _.map(this.state.templates[index]['fields'], (value) => {
+        const fields = this.GetPickedTemplateFields();
+        const jsx = _.map(fields, (value) => {
             return this.DetermineComponent(value);
         });
         return (
@@ -99,12 +151,12 @@ class CreateOutage extends Component {
             <div>
                 <Dashboard />
                 <Paper zDepth={3} style={{padding: '2%'}}>
-                {this.TemplatePicker()}
-                <form>
-                    {this.Form()}
-                </form>
-                <RaisedButton label='Send Outage' />
-            </Paper>
+                    {this.TemplatePicker()}
+                    <form>
+                        {this.Form()}
+                    </form>
+                    <RaisedButton label='Send Outage' />
+                </Paper>
             </div>
         );
     }

af004a6c9858a1381fcec12c0ae05290b8640623
banjocat <jackmuratore@gmail.com>
2017-05-30 00:04:52 -0400

    Add mongoose and switch over to mongodb

--- express/index.js
+++ express/index.js
@@ -1,4 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/test');
+
+var Cat = mongoose.model('Cat',
+    {
+        name: String
+    }
+);
+
+const kitty = new Cat({ name: 'Patrick' });
+
+kitty.save( (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('meow');
+    }
+});
+
+
 const app = express();
 
 const port = 3000;
